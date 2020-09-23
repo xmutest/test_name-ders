5f2d6ce3218ed0b528ea9a9ffa0f7aadf47c2040
doge127 <494901473@163.com>
2020-09-23 11:08:28 +0800

    content样式修改

--- src/views/demo/ControllerLink/paIntls.vue
+++ src/views/demo/ControllerLink/paIntls.vue
@@ -1,16 +1,83 @@
 <!-- 测评工具及接入点说明 -->
 <template>
     <d2-container>
-    <template slot="header">测评工具及接入点说明</template>
-  </d2-container>
+      
+      <div class="mude_is">
+        <!-- 富文本输入框 -->
+        <div class="mude_is_left">
+          <el-card class="box-card">
+            <div class="mude_text_item">
+              <div class="descTItle">接入点描述</div>
+              <d2-quill
+                style="min-height: 200px; margin-bottom: 20px;"
+                v-model="jrdPoint"
+                @text-change="textChangeHandler"
+              />
+            </div>
+
+            <div class="mude_text_item">
+              <div class="descTItle">接入点图片</div>
+              <div class="to_tim">
+                <el-tag>操作提示</el-tag>
+                <span>点击图片后可打开标记JA,JB等标记的页面</span>
+              </div>
+            </div>
+
+          </el-card>
+
+          
+        </div>
+        <!-- 图片 -->
+
+      </div>
+    </d2-container>
 </template>
 
 <script>
 export default {
+  data(){
+    return {
+      jrdPoint:'',
 
+    }
+  },
+  methods: {
+    textChangeHandler(delta, oldDelta, source){
+      // console.log(delta,oldDelta,source)
+    },
+
+  },
 }
 </script>
 
-<style>
-
+<style lang="scss">
+.mude_is {
+  margin: 20px 0;
+  .mude_is_left {
+    margin: 20px 0;
+  }
+  .mude_is_right > div {
+    margin: 20px 0;
+  }
+  .to_tim{
+    margin-top: 5px;
+    .el-tag{
+      margin-right:5px;
+    }
+  }
+  .descTItle{
+    padding: 8px 0px 8px 10px;
+    margin-bottom: 20px;
+    border-left: 4px solid #007AF9;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+  .mude_text_item{
+    border-bottom: 1px solid #EBEEF5;
+    padding:20px 0;
+  }
+}
+.el-card__header{
+    border-bottom: 0px solid #EBEEF5;
+}
 </style>
