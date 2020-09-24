e3c7257f80ad3fb128add10b8d083244f8f9409e
doge127 <494901473@163.com>
2020-09-24 10:45:18 +0800

    添加了安全服务,安全环境页面

--- src/views/demo/ControllerLink/anquanfs.vue
+++ src/views/demo/ControllerLink/anquanfs.vue
@@ -1,16 +1,135 @@
 <!-- 安全服务 -->
 <template>
   <d2-container>
-    <template slot="header">安全服务</template>
+    
+      <el-table :data="tabledatas" border>
+        <el-table-column label="" width="80">
+          <template slot-scope="scope">
+            <div class="itsz"></div>
+            <span v-show="!scope.row.show">{{scope.$index + 1}}</span>
+          </template>
+        </el-table-column>
+
+        <el-table-column label="安全服务名称" width="450">
+          <template slot-scope="scope">
+            <div @click="is_compile(scope.row,scope.$index,'serverName')" class="itsz"></div>
+            <el-input
+              :ref='"serverName"+scope.$index'
+              @blur="schujiaodian(scope.row)"
+              v-show="scope.row.show"
+              v-model="scope.row.serverName"
+            ></el-input>
+            <span v-show="!scope.row.show">{{scope.row.serverName}}</span>
+          </template>
+        </el-table-column>
+
+        <el-table-column label="安全服务商" width="450">
+          <template slot-scope="scope">
+            <div @click="is_compile(scope.row,scope.$index,'serverBussiness')" class="itsz"></div>
+            <el-input :ref='"serverBussiness"+scope.$index' @blur="schujiaodian(scope.row)" placeholder="请输入内容" v-show="scope.row.show" v-model="scope.row.serverBussiness"></el-input>
+            <span v-show="!scope.row.show">{{scope.row.serverBussiness}}</span>
+          </template>
+        </el-table-column>
+
+        <el-table-column fixed="right" label="操作" width="200">
+          <template slot-scope="scope">
+            <!-- <el-button size="mini" @click="is_compile(scope.row)">编辑</el-button> -->
+            <el-button size="mini" @click="is_preserve(scope.$index)">新增</el-button>
+            <el-button size="mini" type="danger" @click="deleteRow(scope.$index, tabledatas)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
   </d2-container>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return{
+        tableData: [{
+            date: '2016-05-02',
+            name: '王小虎',
+          }, {
+            date: '2016-05-04',
+            name: '王小虎',
+          }, {
+            date: '2016-05-01',
+            name: '王小虎',
+          }, {
+            date: '2016-05-03',
+            name: '王小虎',
+        }]
+    }
+    tabledatas:[]
+  },
+  created() {
+    // 设备名称	虚拟设备	系统及版本	品牌及型号	用途	备注	数量	重要程度	测评指导书	测评对象
+    let list = [
+      {
+        serverName: "网站系统维护",
+        serverBussiness: "易宝系统(中国)有限公司",
+      },
+      {
+        serverName: "安全维护",
+        serverBussiness: "北京天融信网络安全技术有限公司",
+      },
+    ];
+    list.forEach((element) => {
+      element["show"] = false;
+    });
+    this.tabledatas = list;
+    // })
+  },
+  methods: {
+    is_compile(item,index,itname) {
+      // console.log(item,index,itname)
+      item.show = true;
+      console.log(itname);
+      setTimeout(() => {
+        this.$refs[itname + index].focus();
+      }, 1);
+      console.log(item);
+    },
+    schujiaodian(item) {
+      item.show = false;
+      console.log(item);
+    },
+    is_preserve(item) {
+      var itss=this.tabledatas;
+      var list =  {
+        serverName: "",
+        serverBussiness: "",
+        show:false
+      };
+      itss.splice(item+1, 0, list);
+     this.tabledatas = itss;
+      // console.log();
+    },
+    deleteRow(index, rows) {
+      console.log(index, rows);
+      rows.splice(index, 1);
+    },
+  },
 }
 </script>
 
-<style>
-
+<style lang="scss">
+  .form{
+    .el-table th>.cell{
+      text-align:center;
+    }
+    .el-table--enable-row-transition .el-table__body td{
+      text-align:center;
+    }
+  }
+  .itsz {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+  }
+  .el-table td, .el-table th.is-leaf{
+    text-align:center;
+  }
 </style>
--- src/views/demo/ControllerLink/anquanhj.vue
+++ src/views/demo/ControllerLink/anquanhj.vue
@@ -1,14 +1,81 @@
 <!-- // 安全环境 -->
 <template>
     <d2-container>
-    <template slot="header">安全环境</template>
-    测试
+      <el-table :data="tabledatas" border>
+        <el-table-column label="" width="80">
+          <template slot-scope="scope">
+            <div class="itsz"></div>
+            <span v-show="!scope.row.show">{{scope.$index + 1}}</span>
+          </template>
+        </el-table-column>
+
+        <el-table-column label="威胁分（子）类" width="450">
+          <template slot-scope="scope">
+            <span v-show="!scope.row.show">{{scope.row.serverName}}</span>
+          </template>
+        </el-table-column>
+
+        <el-table-column label="描述" width="450">
+          <template slot-scope="scope">
+            <span v-show="!scope.row.show">{{scope.row.serverBussiness}}</span>
+          </template>
+        </el-table-column>
+
+        <el-table-column label="威胁赋值" width="150">
+          <template slot-scope="scope"> 
+            <el-select v-model="scope.row.importance" filterable placeholder="请选择">
+              <el-option
+                v-for="item in importance_list"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              ></el-option>
+              
+            </el-select>
+
+          </template>
+        </el-table-column>
+
+        
+      </el-table>
   </d2-container>
 </template>
 
 <script>
 export default {
+  data(){
+    return{
+      importance_list:[
+        { value: 3, label: "高" },
+        { value: 2, label: "中" },
+        { value: 1, label: "低" },
+      ],
+      tabledatas:[],
+    };
+  },
+  created() {
+    let list = [
+      {
+        serverName: "网站系统维护",
+        serverBussiness: "易宝系统(中国)有限公司",
+        importance: '',
+      },
+      {
+        serverName: "安全维护",
+        serverBussiness: "北京天融信网络安全技术有限公司",
+        importance: '',
+      },
+    ];
+    list.forEach((element) => {
+      element["show"] = false;
+    });
+    this.tabledatas = list;
 
+    console.log('选项',this.importanceList)
+  },
+  methods: {
+    
+  },
 }
 </script>
 
