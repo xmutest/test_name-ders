1cdb9043eedac7a8114858fb7d7bf6a6eeb0fc4e
doge127 <494901473@163.com>
2020-10-13 09:04:32 +0800

    测评工具接入点图片编辑功能 初步实现导出为png功能,拖动功能,自适应画布功能

---
+++ src/components/canvasToorBarSel/canvasToorBarSel.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="container">
+        <div class="selectArea">
+            <img :src="require('@/views/demo/ControllerLink/img/close.png')" alt="" class="btnClose">
+            <img :src="require('@/views/demo/ControllerLink/img/palntlsImg/ja1.png')" alt="" class="guidePic1">
+            <img :src="require('@/views/demo/ControllerLink/img/palntlsImg/ja2.png')" alt="" class="guidePic2">
+            <img :src="require('@/views/demo/ControllerLink/img/palntlsImg/ja3.png')" alt="" class="guidePic3">
+            <img :src="require('@/views/demo/ControllerLink/img/palntlsImg/ja4.png')" alt="" class="guidePic4">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    },
+    created() {
+        
+    },
+    methods: {
+        
+    },
+}
+</script>
+
+<style lang="scss">
+    .container{
+        position: fixed;
+        top:0;
+        left:0;
+        width:100vw;
+        height:100vh;
+        background:rgba(0,0,0,.7);
+        z-index:999;
+        .selectArea{
+            width: 600px;
+            height: 500px;
+            background: #fff;
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            margin: auto;
+        }
+        .btnClose{
+            width: 30px;
+            position: absolute;
+            top: 20px;
+            right: 20px;
+            z-index: 10;
+        }
+        .guidePic1,.guidePic3{
+            width: 279px;
+            position: absolute;
+        }
+        .guidePic2,.guidePic4{
+            width:151px;
+            position: absolute;
+        }
+        .guidePic1{
+            top: 100px;
+            left: 50px;
+        }
+        .guidePic2{
+            top: 20px;
+            right: 50px;
+        }
+        .guidePic3{
+            top: 400px;
+            left: 50px;
+        }
+        .guidePic4{
+            top: 270px;
+            right: 50px;
+        }
+    }
+
+    
+</style>
--- src/views/demo/ControllerLink/paIntls.vue
+++ src/views/demo/ControllerLink/paIntls.vue
@@ -19,20 +19,22 @@
             <div class="to_tim">
               <el-tag>操作提示</el-tag>
               <span>点击图片后可打开标记JA,JB等标记的页面</span>
-              <div class="canvasArea" ref="canvasArea">
-                <div class="toorBar">
+              
+              <div class="toorBar">
+                  <el-button type="primary">JA</el-button>
+                  <el-button type="primary">JB</el-button>
+                  <el-button type="primary">JC</el-button>
+                  <el-button type="primary">JD</el-button>
+                  <el-button type="primary">JE</el-button>
+                  <el-button type="primary">JF</el-button>
+              </div>
 
-                </div>
-                <easel-canvas :width="cas.width" :height="cas.height">
-                  <!-- <easel-shape
-                      :x="200"
-                      :y="150"
-                      form="circle"
-                      fill="blue"
-                      :dimensions="20"
-                      :align="['center','center']"
-                  >
-                  </easel-shape> -->
+              <canvasToorBarSel></canvasToorBarSel>
+
+              <div class="canvasArea" ref="canvasArea">
+                <easel-canvas :width="cas.width" :height="cas.height" ref="canvaspic">
+                <!-- <canvas :width="cas.width" :height="cas.height" ref="canvaspic"> -->
+                  
                   <!-- 使用图片要用require获取 -->
                   <easel-bitmap
                     :image="require('@/views/demo/ControllerLink/img/structure01.jpg')"
@@ -41,10 +43,33 @@
                     :align="['center','top']"
                   >
                   </easel-bitmap>
-
+                  <easel-bitmap
+                    :image="require('@/views/demo/ControllerLink/img/palntlsImg/ja1.png')"
+                    :x="0"
+                    :y="0"
+                    :align="['left','top']"
+                    @mousedown="pointEvent($event)"
+                    @pressmove.prevent="objMove($event)"
+                    @mouseout="objOut($event)"
+                  >
+                  </easel-bitmap>
+                  <!-- <easel-shape
+                      :x="200"
+                      :y="150"
+                      form="circle"
+                      fill="blue"
+                      :dimensions="20"
+                      :align="['center','center']"
+                      @mousedown="pointEvent($event)"
+                      @pressmove.prevent="objMove($event)"
+                      @mouseout="objOut($event)"
+                  >
+                  </easel-shape> -->
+                <!-- </canvas> -->
                 </easel-canvas>
               </div>
-              
+              导出的结果图:
+              <img src="" alt="" class="resPic">
             </div>
           </div>
         </el-card>
@@ -55,7 +80,7 @@
 </template>
 
 <script>
-
+import canvasToorBarSel from '@/components/canvasToorBarSel/canvasToorBarSel'
 export default {
   data() {
     return {
@@ -64,14 +89,28 @@ export default {
         width:0,
         height:0
       },
-      
+      mouseInfo:{
+        startX:0,
+        startY:0,
+        moveX:0,
+        moveY:0,
+        startLocateX:0,
+        startLocateY:0
+      }
     };
+  },
+  components:{
+    canvasToorBarSel
   },
   created() {
     
   },
   mounted() {
+    let that = this
     this.casSize()
+    setTimeout(function(){
+      that.outputPic()
+    },1000)
   },
   computed: {
     
@@ -94,6 +133,104 @@ export default {
       this.cas.width = parseInt(that.getStyle(casBox, 'width'));
       this.cas.height = parseInt(that.getStyle(casBox, 'height'));
     },
+    outputPic(){
+      // console.log('canvaspic',this.$refs.canvaspic.$el)
+      // canvas转base64图片
+      let parper = this.$refs.canvaspic.$el
+      var result = parper.toDataURL("image/png");
+
+      // 获取获取图片
+      let resPic = document.querySelector('.resPic')
+      resPic.src = result
+      // console.log(result)
+    },
+    pointEvent(e){
+
+      // 触发事件对象
+      let obj = e.currentTarget
+      console.log('对象',obj)
+      
+      // 对象的位置
+      // obj.x = 400
+      // obj.y = 100
+
+      this.mouseInfo.startLocateX = obj.x
+      this.mouseInfo.startLocateY = obj.y
+
+      this.mouseInfo.startX = e.stageX
+      this.mouseInfo.startY = e.stageY
+
+    },
+    objMove(e){
+      // console.log('mouseMove',e)
+      this.mouseInfo.moveX = e.stageX
+      this.mouseInfo.moveY = e.stageY
+
+      let diveceX = this.mouseInfo.moveX - this.mouseInfo.startX
+      let diveceY = this.mouseInfo.moveY - this.mouseInfo.startY
+
+
+      // console.log('距离变化',diveceX,diveceY)
+
+      // 触发事件对象
+      let obj = e.currentTarget
+      console.log('对象',e)
+
+      
+      
+
+      // 获取图片宽高
+      let imgWidth = obj.image.width
+      let imgHeight = obj.image.height
+
+      // console.log(obj.x,obj.y)
+
+      // if(obj.x < 0 ){
+      //   obj.x = 0
+      // }else if(obj.x + imgWidth > this.cas.width){
+      //   obj.x = this.cas.width - imgWidth
+      // }else{
+      //   obj.x = this.mouseInfo.startLocateX + diveceX
+      // }
+
+      // console.log(this.cas.width)
+
+      // console.log(this.cas)
+
+      obj.x = this.mouseInfo.startLocateX + diveceX
+      obj.y = this.mouseInfo.startLocateY + diveceY
+
+      if(obj.x < 0 || obj.x + imgWidth > this.cas.width){
+        if(obj.x < 0){
+          obj.x = 0
+        }else{
+          obj.x = this.cas.width - imgWidth
+        }
+      }
+
+      // consoe.log(obj.x,obj.y)
+
+      if(obj.y < 0 || obj.y + imgHeight > this.cas.height){
+        if(obj.y < 0){
+          obj.y = 0
+        }else{
+          obj.y = this.cas.height - imgHeight
+        }
+      }
+
+
+      // console.log(this.mouseInfo)
+
+      // this.mouseInfo.moveX
+    },
+    objOut(e){
+      // 触发事件对象
+      let obj = e.currentTarget
+      console.log('对象',obj)
+
+      this.mouseInfo.startLocateX = obj.x
+      this.mouseInfo.startLocateY = obj.y
+    }
   },
 };
 </script>
@@ -124,5 +261,10 @@ export default {
   width:100%;
   height:400px;
   border:1px solid #ccc;
+}
+.toorBar{
+  width:100%;
+  padding:0 20px;
+  margin-bottom:20px;
 }
 </style>
