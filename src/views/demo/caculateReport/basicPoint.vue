ee41887bd974bc0ba35f72392436c9edc127c395
doge127 <494901473@163.com>
2020-10-29 08:54:41 +0800

    统计和报告->单项指标页面ui制作

--- src/views/demo/caculateReport/basicPoint.vue
+++ src/views/demo/caculateReport/basicPoint.vue
@@ -1,17 +1,258 @@
 <!--基本指标-->
 <template>
   <d2-container>
-    <template slot="header">基本指标</template>
-    基本指标
+    <div class="mude_is">
+      <el-card class="box-card">
+        <el-table
+          :data="tableImitateDate"
+          :span-method="objectSpanMethod"
+          border
+          show-summary
+          sum-text="安全通用要求"
+          style="width: 100%; margin-top: 20px"
+        >
+          <el-table-column
+            prop="groName"
+            label="安全类"
+            width="180"
+            max-height="100"
+          >
+            <template slot-scope="scope">
+              <div :style="scope.row.objNum==438?'background:red':''">
+                {{ scope.row.groName }}
+              </div>
+            
+            </template>
+          </el-table-column>
+          <el-table-column prop="name" label="安全控制点"> </el-table-column>
+          <el-table-column prop="cpxs" label="测评项数"> </el-table-column>
+          <el-table-column prop="objNum" label="测评对象数"> </el-table-column>
+          <el-table-column prop="totalNum" label="测评项总数">
+          </el-table-column>
+        </el-table>
+      </el-card>
+    </div>
   </d2-container>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      tableData: [
+        {
+          id: "12987122",
+          name: "王小虎",
+          amount1: "234",
+          amount2: "3.2",
+          amount3: 10,
+        },
+        {
+          id: "12987123",
+          name: "王小虎",
+          amount1: "165",
+          amount2: "4.43",
+          amount3: 12,
+        },
+        {
+          id: "12987124",
+          name: "王小虎",
+          amount1: "324",
+          amount2: "1.9",
+          amount3: 9,
+        },
+        {
+          id: "12987125",
+          name: "王小虎",
+          amount1: "621",
+          amount2: "2.2",
+          amount3: 17,
+        },
+        {
+          id: "12987126",
+          name: "王小虎",
+          amount1: "539",
+          amount2: "4.1",
+          amount3: 15,
+        },
+      ],
+      imitateData: [
+        {
+          groupName: "安全物理环境",
+          group: [
+            {
+              name: "物理位置选择",
+              cpxs: 211,
+              objNum: 431,
+              totalNum: 1411,
+            },
+            {
+              name: "物理访问控制",
+              cpxs: 211,
+              objNum: 431,
+              totalNum: 1411,
+            },
+            {
+              name: "防盗窃和防破坏",
+              cpxs: 211,
+              objNum: 431,
+              totalNum: 1411,
+            },
+            {
+              name: "防雷击",
+              cpxs: 211,
+              objNum: 431,
+              totalNum: 1411,
+            },
+          ],
+        },
+        {
+          groupName: "安全通信网络",
+          group: [
+            {
+              name: "网络架构",
+              cpxs: 211,
+              objNum: 431,
+              totalNum: 1411,
+            },
+            {
+              name: "通信传输",
+              cpxs: 211,
+              objNum: 431,
+              totalNum: 1411,
+            },
+            {
+              name: "可信验证",
+              cpxs: 211,
+              objNum: 431,
+              totalNum: 1411,
+            },
+          ],
+        },
+        {
+          groupName: "安全区域边界",
+          group: [
+            {
+              name: "边界防护",
+              cpxs: 211,
+              objNum: 431,
+              totalNum: 1411,
+            },
+            {
+              name: "访问控制",
+              cpxs: 211,
+              objNum: 431,
+              totalNum: 1411,
+            },
+            {
+              name: "入侵防范",
+              cpxs: 211,
+              objNum: 431,
+              totalNum: 1411,
+            },
+            {
+              name: "恶意代码和垃圾邮件防范",
+              cpxs: 211,
+              objNum: 431,
+              totalNum: 1411,
+            },
+          ],
+        },
+        {
+          groupName: "安全计算环境",
+          group: [
+            {
+              name: "身份鉴别",
+              cpxs: 211,
+              objNum: 431,
+              totalNum: 1411,
+            },
+            {
+              name: "数据完整性",
+              cpxs: 211,
+              objNum: 431,
+              totalNum: 1411,
+            },
+            {
+              name: "数据保密性",
+              cpxs: 211,
+              objNum: 431,
+              totalNum: 1411,
+            },
+          ],
+        },
+      ], //模拟数据
+      tableImitateDate: [],
+      tableImitateIndex: [],
+    };
+  },
+  created() {
+    // this.func_get_config()
+  },
+  mounted() {
+    this.arrengeData();
+  },
+  methods: {
+    arrengeData() {
+      let imitateData = this.imitateData;
+      let groupIndex = 0;
+      for (let i = 0; i < imitateData.length; i++) {
+        let groupName = imitateData[i].groupName;
+        this.tableImitateIndex.push(groupIndex);
+        groupIndex += imitateData[i].group.length;
+        for (let j = 0; j < imitateData[i].group.length; j++) {
+          let paramsJson = {
+            groName: groupName,
+            name: imitateData[i]["group"][j].name,
+            cpxs: imitateData[i]["group"][j].cpxs,
+            objNum: imitateData[i]["group"][j].objNum,
+            totalNum: imitateData[i]["group"][j].totalNum,
+          };
 
-}
+          // 处理插入的数据
+          this.tableImitateDate.push(paramsJson);
+          // tableImitateIndex
+        }
+      }
+      this.tableImitateIndex.push(this.tableImitateDate.length);
+
+      //将合并数量插入到对应合并首行的数据中
+      for (let i = 0; i < this.tableImitateIndex.length - 1; i++) {
+        this.tableImitateDate[this.tableImitateIndex[i]].rowspan =
+          this.tableImitateIndex[i + 1] - this.tableImitateIndex[i];
+      }
+
+      console.log(this.tableImitateDate);
+      console.log(this.tableImitateIndex);
+    },
+    objectSpanMethod({ row, column, rowIndex, columnIndex }) {
+      for (let i = 0; i < this.tableImitateIndex.length - 1; i++) {
+        // 仅限第一行
+        if (columnIndex === 0) {
+          if (row.rowspan >= 1) {
+            return {
+              rowspan: row.rowspan,
+              colspan: 1,
+            };
+          } else {
+            return {
+              rowspan: 0,
+              colspan: 0,
+            };
+          }
+        }
+      }
+    },
+    async func_get_config() {
+      let res = await this.$api.API_ProjectfindOverallEvaluation();
+      console.log(res);
+    },
+  },
+};
 </script>
 
-<style>
-
+<style lang="scss">
+.mude_is {
+  margin: 20px 0;
+}
 </style>
--- src/views/demo/caculateReport/dxtj.vue
+++ src/views/demo/caculateReport/dxtj.vue
@@ -1,17 +1,274 @@
-<!--单项符合情况统计-->
+<!--基本指标-->
 <template>
   <d2-container>
-    <template slot="header">单项符合情况统计</template>
-    单项符合情况统计
+    <div class="mude_is">
+        <el-card class="box-card">
+              <el-table
+                :data="tableImitateDate"
+                :span-method="objectSpanMethod"
+                border
+                show-summary
+                sum-text="安全通用要求"
+                style="width: 100%; margin-top: 20px"
+              >
+                <el-table-column
+                  prop="groName"
+                  label="安全类"
+                  width="180"
+                  max-height="100"
+                >
+                </el-table-column>
+                <el-table-column
+                  prop="name"
+                  label="安全控制点">
+                </el-table-column>
+                <el-table-column
+                  prop="cpxs"
+                  label="测评项数">
+                </el-table-column>
+                <el-table-column
+                  prop="objNum"
+                  label="测评对象数">
+                </el-table-column>
+                <el-table-column
+                  prop="totalNum"
+                  label="测评项总数">
+                </el-table-column>
+              </el-table>
+        </el-card>
+    </div>
   </d2-container>
 </template>
 
 <script>
 export default {
+  data(){
+    return {
+      tableData: [{
+          id: '12987122',
+          name: '王小虎',
+          amount1: '234',
+          amount2: '3.2',
+          amount3: 10
+        }, {
+          id: '12987123',
+          name: '王小虎',
+          amount1: '165',
+          amount2: '4.43',
+          amount3: 12
+        }, {
+          id: '12987124',
+          name: '王小虎',
+          amount1: '324',
+          amount2: '1.9',
+          amount3: 9
+        }, {
+          id: '12987125',
+          name: '王小虎',
+          amount1: '621',
+          amount2: '2.2',
+          amount3: 17
+        }, {
+          id: '12987126',
+          name: '王小虎',
+          amount1: '539',
+          amount2: '4.1',
+          amount3: 15
+        }],
+      imitateData:[
+        {
+          groupName:'安全物理环境',
+          group:[
+            {
+              name:'物理位置选择',
+              item:[
+                {
+                  itemName:`a)机房场地应选择...`,
+                  cpobjNum:2,
+                  pass:`20%`,
+                  halfPass:'0%',
+                  noPass:'50%',
+                  notUse:'0'
+                },
+                {
+                  itemName:`b)避免设在建筑物的顶层`,
+                  cpobjNum:2,
+                  pass:`20%`,
+                  halfPass:'0%',
+                  noPass:'50%',
+                  notUse:'0'
+                },
+                {
+                  itemName:`z)zzzzz`,
+                  cpobjNum:2,
+                  pass:`20%`,
+                  halfPass:'0%',
+                  noPass:'50%',
+                  notUse:'0'
+                },
+              ],
+              cpxs:211,
+              objNum:431,
+              totalNum:1411,
+            },
+            {
+              name:'物理访问控制',
+              item:[
+                {
+                  itemName:`x)xxxxxxx...`,
+                  cpobjNum:2,
+                  pass:`20%`,
+                  halfPass:'0%',
+                  noPass:'50%',
+                  notUse:'0'
+                },
+                {
+                  itemName:`v)vvvvvvv`,
+                  cpobjNum:2,
+                  pass:`20%`,
+                  halfPass:'0%',
+                  noPass:'50%',
+                  notUse:'0'
+                },
+              ],
+              cpxs:211,
+              objNum:431,
+              totalNum:1411,
+            },
+            {
+              name:'防盗窃和防破坏',
+              item:[
+                {
+                  itemName:`e)鹅鹅鹅曲项向天歌...`,
+                  cpobjNum:2,
+                  pass:`20%`,
+                  halfPass:'0%',
+                  noPass:'50%',
+                  notUse:'0'
+                },
+                {
+                  itemName:`f)避免设在建筑物的顶层`,
+                  cpobjNum:2,
+                  pass:`20%`,
+                  halfPass:'0%',
+                  noPass:'50%',
+                  notUse:'0'
+                },
+              ],
+              cpxs:211,
+              objNum:431,
+              totalNum:1411,
+            },
+            {
+              name:'防雷击',
+              item:[
+                {
+                  itemName:`a)aaAaaaAAAaa...`,
+                  cpobjNum:2,
+                  pass:`20%`,
+                  halfPass:'0%',
+                  noPass:'50%',
+                  notUse:'0'
+                },
+                {
+                  itemName:`b)bbbbbb`,
+                  cpobjNum:2,
+                  pass:`20%`,
+                  halfPass:'0%',
+                  noPass:'50%',
+                  notUse:'0'
+                },
+              ],
+              cpxs:211,
+              objNum:431,
+              totalNum:1411,
+            },
+          ]
+        },
+      ],  //模拟数据
+      tableImitateDate:[],
+      tableImitateIndex:[],
+      tableImitateItemIndex:[],
+    }
+  },
+  created() {
+    // this.func_get_config()
+  },
+  mounted() {
+    this.arrengeData()
+  },
+  methods: {
+    arrengeData(){
+      let imitateData = this.imitateData
+      let groupIndex = 0,groupLength = 0
+      for(let i=0;i<imitateData.length;i++){
+        let groupName = imitateData[i].groupName
+        this.tableImitateIndex.push(groupIndex)
+        groupIndex += imitateData[i].group.length
 
+        groupLength += imitateData[i].group.length
+
+        for(let j=0;j<imitateData[i].group.length;j++){
+          let name = imitateData[i]['group'][j].name
+          for(let k=0;k<imitateData[i]['group'][j].item.length;k++){
+            
+            let paramsJson = {
+              groName:groupName,
+              name:name,
+              itemName:imitateData[i]['group'][j]['item'][k].itemName,
+              cpobjNum:imitateData[i]['group'][j].cpxs,
+              pass:imitateData[i]['group'][j].objNum,
+              halfPass:imitateData[i]['group'][j].totalNum,
+              noPass:imitateData[i]['group'][j].totalNum,
+              notUse:imitateData[i]['group'][j].totalNum,
+            }
+            // 处理插入的数据
+            this.tableImitateDate.push(paramsJson)
+          }
+          // tableImitateIndex
+        }
+      }
+        this.tableImitateIndex.push(groupLength)
+
+        //将合并数量插入到对应合并首行的数据中
+        for(let i=0;i<this.tableImitateIndex.length - 1;i++){
+          this.tableImitateDate[this.tableImitateIndex[i]].rowspan = this.tableImitateIndex[i+1] - this.tableImitateIndex[i]
+        }
+
+      
+
+        console.log(this.tableImitateDate)
+        console.log(this.tableImitateIndex)
+    },
+    objectSpanMethod({ row, column, rowIndex, columnIndex }) {
+      for(let i=0;i<this.tableImitateIndex.length - 1;i++){
+        // 仅限第一行
+        if(columnIndex === 0){
+          if(row.rowspan>=1){
+            return{
+              rowspan:row.rowspan,
+              colspan:1
+            }
+          }else{
+            return {
+              rowspan:0,
+              colspan:0
+            }
+          }
+          
+        }
+      }
+    },
+    async func_get_config(){
+      let res = await this.$api.API_ProjectfindOverallEvaluation()
+      console.log(res)
+    }
+  },
 }
 </script>
 
-<style>
-
+<style lang="scss">
+  .mude_is {
+    margin:20px 0;
+  }
 </style>
