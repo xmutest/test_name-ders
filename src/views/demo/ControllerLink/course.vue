f7e803e4ad41e28fd485c80cbbd10e80a6735afe
doge127 <494901473@163.com>
2021-02-09 15:42:43 +0800

    模版文字修改

--- src/views/demo/ControllerLink/course.vue
+++ src/views/demo/ControllerLink/course.vue
@@ -755,8 +755,11 @@ export default {
         }
       }
 
+      console.log('table',table)
+
       // console.log('保存的数据',table)
       this.submitReport()
+
       this.apiCreupdata(table);
     },
     // 修改或者保存
@@ -986,7 +989,7 @@ export default {
 
         console.log(this.addZeroOfDate(new Date(realityTime[0].startTime).getMonth() + 1))
 
-        this.sentences = `本次等级测评分为四个过程：测评准备过程、方案编制过程、测评实施过程、分析与报告编制过程。其中，各阶段的时间安排如下：
+        this.sentences = `本次等级测评分为四个过程：测评准备过程、方案编制过程、测评实施过程、分析与报告编制过程。具体如图1.1所示。其中，各阶段的时间安排如下：
     1、${new Date(realityTime[0].startTime).getFullYear()}年${
           this.addZeroOfDate(new Date(realityTime[0].startTime).getMonth() + 1)
         }月${this.addZeroOfDate(new Date(realityTime[0].startTime).getDate())}日~${new Date(
@@ -1018,7 +1021,7 @@ export default {
           realityTime[3].endTime
         ).getDate())}日，分析与报告编制过程。`;
       } else {
-        this.sentences = `本次等级测评分为四个过程：测评准备过程、方案编制过程、测评实施过程、分析与报告编制过程。其中，各阶段的时间安排如下：
+        this.sentences = `本次等级测评分为四个过程：测评准备过程、方案编制过程、测评实施过程、分析与报告编制过程。具体如图1.1所示。其中，各阶段的时间安排如下：
     1、YYYY年MM月DD日~YYYY年MM月DD日，测评准备阶段。
     2、YYYY年MM月DD日~YYYY年MM月DD日，方案编制过程。
     3、YYYY年MM月DD日~YYYY年MM月DD日，现场实施过程。
