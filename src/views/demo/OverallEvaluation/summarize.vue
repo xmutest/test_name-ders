db66dbf5e54c10141d05bd340b90181e39185b9b
YuanMeiG <645095460@qq.com>
2022-04-28 09:03:02 +0800

    提交概述页面

---
+++ src/views/demo/OverallEvaluation/summarize.vue
@@ -0,0 +1,262 @@
+<!--概述-->
+<template>
+  <d2-container>
+    <el-form ref="ruleForm" :model="form" label-width="110px" :rules="rules" class="mainForm">
+      <el-alert title="评估报告概述" type="success" :closable="false"></el-alert>
+      <el-form-item label="评估起止日期" prop="time">
+        <el-date-picker
+            value-format="yyyy-MM-dd"
+            v-model="form.time"
+            type="daterange"
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期">
+          </el-date-picker>
+      </el-form-item>
+      <el-form-item label="评估结论" prop="result">
+        <el-input size="medium" v-model="form.result" type="textarea" :rows="1"></el-input>
+      </el-form-item>
+      <el-form-item label="评估概述" prop="summary">
+        <el-input size="medium" v-model="form.summary" type="textarea" :rows="1"></el-input>
+      </el-form-item>
+      <div class="btnBox">
+        <el-button type="success" size="small" @click="submitForm('ruleForm')">保存</el-button>
+      </div>
+      <el-alert title="评估人员构成" type="success" :closable="false"></el-alert>  
+      <el-table
+        border
+        :data="riskAssessorTable"
+        style="width: 100%">
+        <el-table-column label="序号" type="index" width="50"></el-table-column>
+        <el-table-column prop="name" label="姓名" width="180">
+          <template slot-scope="scope">
+            <el-input size="mini" v-model="scope.row.name" clearable :disabled="!scope.row.isEdited"></el-input>
+          </template>
+        </el-table-column>
+        <el-table-column prop="role" label="项目角色" width="180">
+          <template slot-scope="scope">
+            <el-select size="mini" v-model="scope.row.role" placeholder="选择角色" :disabled="!scope.row.isEdited">
+              <el-option v-for="item in roleList" :label="item" :value="item" :key="item"></el-option>
+            </el-select>
+          </template>
+        </el-table-column>
+        <el-table-column prop="content" label="参与内容">
+          <template slot-scope="scope">
+            <el-input size="mini" v-model="scope.row.content" :disabled="!scope.row.isEdited"></el-input>
+          </template>
+        </el-table-column>
+        <el-table-column fixed="right" label="操作" width="180">
+          <template slot="header" slot-scope="scope">
+            <el-button @click="addRoleBtn(scope.row)" type="success" size="mini">新增</el-button>
+          </template>
+          <template slot-scope="scope">
+            <el-button v-if="!scope.row.isEdited" type="default" size="mini" @click="editRoleBtn(scope.row,scope.$index)">编辑</el-button>
+            <el-button v-if="scope.row.isEdited" type="default" size="mini" @click="saveRoleBtn(scope.row)">保存</el-button>
+            <el-button @click="delRoleBtn(scope.row,scope.$index)" type="danger" size="mini">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      
+    </el-form>
+  </d2-container>
+</template>
+
+<script>
+  function onlyNum(value) {
+    value=value.replace(/[^0-9.]/g,'')
+  }
+export default {
+	data() {
+	    return {
+        
+	    	form:{
+            time:[],
+            beginTime:'',
+            endTime:'',
+            result:'',
+            summary:'',
+	    	},
+	    	rules:{
+	    	  time:[
+            { required: true, message: '请输入评估起止日期', trigger: 'blur' },
+	    	  ],
+          result:[
+            { required: true, message: '请输入评估结论', trigger: 'blur' },
+          ],
+          summary:[
+            { required: true, message: '请输入评估概述', trigger: 'blur' },
+          ],
+	    	},
+        riskAssessorSave:true,//true就执行save false就执行update
+        riskAssessorTable:[],
+        roleList:['甲方项目负责人','乙方项目负责人','甲方项目联络人','项目经理','应用系统检测','文档管理','甲方相关人员',]
+      
+	   }
+	},
+	created() {
+    this.getProjectId();
+    this.getDetail();
+    this.getEvaluateUser();
+
+    
+	},
+  mounted() {
+
+  },
+	methods: {
+    async getProjectId(){
+      var projectId = await this.$api.rickReport_ProjectId();
+      this.projectId = projectId;
+    },
+    async getDetail() {//获取概述详情
+
+      let res = await this.$api.rickReport_ProjectDetail();
+      // console.log(res);
+      if(res.code==20000){
+        res.data.time = [res.data.endTime,res.data.endTime]
+        this.form = res.data;
+      }else{
+        this.$message.error(res.messager);
+      }
+
+    },
+    async saveDetail() {//保存概述详情
+
+      this.form.beginTime = this.form.time[0]
+      this.form.endTime = this.form.time[1]
+
+      let res = await this.$api.rickReport_SaveProjectDetail(this.form);
+      // console.log(res);
+      if(res.code==20000){
+        // this.form = res.data;
+        this.$message.success('保存概述成功');
+      }else{
+        this.$message.error(res.messager);
+      }
+
+    },
+    async getEvaluateUser() {//获取人员列表
+      let res = await this.$api.rickReport_EvaluateUser();
+      // console.log(res);
+      if(res.code==20000){
+
+        if(res.data.length!=0){
+          res.data.forEach((item,index)=>{
+            item.isEdited = 0;
+          })
+          this.riskAssessorTable = res.data;
+          this.riskAssessorSave =false;
+        }else{
+          this.riskAssessorTable.push({name: '',role: '',content: '',isAdd:1,projectId:this.projectId,isEdited:1})
+        }
+      }else{
+        this.$message.error(res.messager);
+      }
+    },
+    addRoleBtn(){//新增人员按钮
+      this.riskAssessorTable.push({name: '',role: '',content: '',isAdd:1,projectId:this.projectId,isEdited:1})
+    },
+    editRoleBtn(row,index){//编辑人员按钮
+      this.riskAssessorTable[index].isEdited = 1;
+      // console.log(this.riskAssessorTable)
+    },
+    saveRoleBtn(row,index){//保存人员按钮
+      console.log(row.isAdd)
+      if(row.isAdd){
+        this.saveRole(row,index)
+      }else{
+        this.updateRole(row,index)
+      }
+    },
+    async saveRole(row,index){//保存人员（单个）
+
+      let res = await this.$api.rickReport_EvaluateUserSave([row]);
+      if(res.code==20000){
+        this.$message.success('新增人员成功！');
+        this.getEvaluateUser();
+      }else{
+        this.$message.error(res.messager);
+      }
+    },
+    async updateRole(row,index){//更新人员信息（单个）
+      let res = await this.$api.rickReport_EvaluateUserUpdate(row);
+      // console.log(res);
+      if(res.code==20000){
+        this.$message.success('修改人员成功！');
+        this.getEvaluateUser();
+      }else{
+        this.$message.error(res.messager);
+      }
+    },
+    delRoleBtn(row,index){//删除人员按钮
+      console.log(row)
+      this.$confirm('确定要删除吗?', '信息', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          if(row.isAdd==1){
+            this.$message.success('删除成功！');
+            this.riskAssessorTable.splice(index,1)
+          }else{
+            this.delRole(row,index);
+          }
+        })
+        .catch(() => {})
+      
+    },
+    async delRole(row,index){//删除人员
+      let res = await this.$api.rickReport_EvaluateUserDel({id:row.id});
+      // console.log(res);
+      if(res.code==20000){
+        this.$message.success('删除成功！');
+        this.riskAssessorTable.splice(index,1)
+      }else{
+        this.$message.error(res.messager);
+      }
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+      if (valid) {
+
+        this.saveDetail();//保存概述
+      } else {
+        console.log('error submit!!');
+        return false;
+      }
+    });
+  },
+	},
+
+}
+
+
+</script>
+
+<style lang="scss" scoped>
+.mainForm{
+  // max-width: 1000px;
+}
+.el-alert{
+  margin-bottom: 20px;
+}
+>>>.el-input__inner{
+  padding:0 5px;
+  text-align: center;
+}
+.btnBox{
+  float: right;
+  button{
+    margin: 10px 0 20px;
+  }
+}
+.el-input.is-disabled .el-input__inner{
+  background-color: #fff!important;
+  border-color: #fff;
+  color:#999;
+}
+.el-input.is-disabled .el-input__icon{
+  display: none;
+}
+</style>
